684a29beda28f8e27e204443cd5e44cc1628b025
yeiserJimenez <yeiser192@gmail.com>
2024-05-23 10:48:52 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,37 @@ import * as net from 'node:net';
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
+        console.log(request);
         const path = request.split(' ')[1];
-        const response = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
-        socket.write(response);
-        socket.end();
+        console.log(path.split('/')[1])
+        const params = path.split('/')[1];
+        let response: string;
+
+        function changeResponse(response: string): void {
+            socket.write(response);
+            socket.end();
+        }
+
+        switch (params) {
+            case '': {
+                response = 'HTTP/1.1 200 OK\r\n\r\n'
+                changeResponse(response)
+                break;
+            }
+            case 'echo': {
+                const message = path.split('/')[2]
+                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
+                changeResponse(response)
+                break;
+            }
+            default: {
+                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+                changeResponse(response)
+                break;
+            }
+        }
+
+        socket.end()
     })
 });
 
